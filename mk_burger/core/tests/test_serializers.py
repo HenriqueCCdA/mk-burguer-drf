be268e75c5e8e2df838e4a2419fe42d24466ebd0
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-28 02:15:51 -0300

    [REFAC: #11] - Criando um serializer para os Ingredientes.
    close #11

---
+++ mk_burger/core/tests/test_serializers.py
@@ -0,0 +1,38 @@
+from mk_burger.core.serializers import (
+    BreadSerializer,
+    IngredientSerializer,
+    MeatSerializer,
+    OptionalSerializer,
+)
+
+
+def test_serializer_breads(bread_list):
+    serializer = BreadSerializer(instance=bread_list, many=True)
+
+    for d, e in zip(bread_list, serializer.data):
+        assert e["id"] == d.id
+        assert e["tipo"] == d.tipo
+
+
+def test_serializer_meats(meat_list):
+    serializer = MeatSerializer(instance=meat_list, many=True)
+
+    for d, e in zip(meat_list, serializer.data):
+        assert e["id"] == d.id
+        assert e["tipo"] == d.tipo
+
+
+def test_serializer_optionais(optionais):
+    serializer = OptionalSerializer(instance=optionais, many=True)
+
+    for d, e in zip(optionais, serializer.data):
+        assert e["id"] == d.id
+        assert e["tipo"] == d.tipo
+
+
+def test_serializer_igredientes(bread_list, meat_list, optionais):
+    serializer = IngredientSerializer({"paes": bread_list, "carnes": meat_list, "opcionais": optionais})
+
+    assert len(serializer.data["paes"]) == len(bread_list)
+    assert len(serializer.data["carnes"]) == len(meat_list)
+    assert len(serializer.data["opcionais"]) == len(optionais)
--- mk_burger/core/urls.py
+++ mk_burger/core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 
-from .views import RootView, ingredientes_list
+from mk_burger.core.views import ingredientes_list
 
 app_name = "core"
 urlpatterns = [
-    path("", RootView.as_view(), name="root"),
     path("ingredientes/", ingredientes_list, name="ingredientes"),
 ]
