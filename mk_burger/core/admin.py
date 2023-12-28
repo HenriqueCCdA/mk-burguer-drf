53aca7392d2bc0144878e3cbcba0d12ef08fc140
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-28 00:39:50 -0300

    [FEAT: #8] - Configurando o Admin dos ingredientes.
    close #8

--- mk_burger/core/admin.py
+++ mk_burger/core/admin.py
@@ -1,1 +1,66 @@
-# Register your models here.
+from django.contrib import admin
+
+from mk_burger.core.models import Bread, Meat, Optional
+
+
+@admin.register(Bread)
+class BreadAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "tipo",
+        "is_active",
+        "created_at",
+        "created_at",
+    )
+
+    list_filter = ("is_active",)
+
+    search_fields = ("tipo",)
+
+    readonly_fields = (
+        "id",
+        "created_at",
+        "modified_at",
+    )
+
+
+@admin.register(Meat)
+class MeatdAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "tipo",
+        "is_active",
+        "created_at",
+        "created_at",
+    )
+
+    list_filter = ("is_active",)
+
+    search_fields = ("tipo",)
+
+    readonly_fields = (
+        "id",
+        "created_at",
+        "modified_at",
+    )
+
+
+@admin.register(Optional)
+class OptionalAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "tipo",
+        "is_active",
+        "created_at",
+        "created_at",
+    )
+
+    list_filter = ("is_active",)
+
+    search_fields = ("tipo",)
+
+    readonly_fields = (
+        "id",
+        "created_at",
+        "modified_at",
+    )
