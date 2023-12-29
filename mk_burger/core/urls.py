dfc0f7796f65a5e71b509c30a7862e5a821e3771
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-29 02:00:08 -0300

    [FEAT: #21] - Criar rota para deletar e atualizar um hamburgue.
    close #21

--- mk_burger/core/urls.py
+++ mk_burger/core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from mk_burger.core.views import burger_lc, ingredientes_list, status_list
+from mk_burger.core.views import burger_lc, burger_rud, ingredientes_list, status_list
 
 app_name = "core"
 urlpatterns = [
     path("ingredientes/", ingredientes_list, name="ingredientes"),
     path("status/", status_list, name="status"),
-    path("burger/", burger_lc, name="burger_lc"),
+    path("burger/", burger_lc, name="burger-list-create"),
+    path("burger/<int:pk>", burger_rud, name="burger-retrieve-update-destroy"),
 ]
