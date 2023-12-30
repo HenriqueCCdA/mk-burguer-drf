22c97a7360f922984a08084fc2ce058f50f35b22
Henrique C. C. de Andrade <henrique.ccda@gmail.com>
2023-12-29 22:18:15 -0300

    [DOC: #22] - Melhorar a documentação nas rotas dos hamburguers.
    close #22

--- mk_burger/core/views.py
+++ mk_burger/core/views.py
@@ -15,9 +15,9 @@ class IngredientesList(APIView):
     def get(self, request):
         """Lista os igredientes disponiveis na plataforma"""
 
-        bread_list = Bread.objects.all()
-        meat_list = Meat.objects.all()
-        optional_list = Optional.objects.all()
+        bread_list = Bread.objects.filter(is_active=True)
+        meat_list = Meat.objects.filter(is_active=True)
+        optional_list = Optional.objects.filter(is_active=True)
 
         serializer = IngredientSerializer(
             {
@@ -31,17 +31,66 @@ class IngredientesList(APIView):
 
 class StatusList(ListAPIView):
     serializer_class = StatusSerializer
-    queryset = Status.objects.all()
+    queryset = Status.objects.filter(is_active=True)
+
+    @extend_schema(
+        summary="Lista dos status",
+    )
+    def get(self, request, *args, **kwargs):
+        """Retorna os status disponiveis na plataforma"""
+        return super().get(request, *args, **kwargs)
 
 
 class BurgerLC(ListCreateAPIView):
     serializer_class = BurgerSerializer
-    queryset = Burger.objects.all()
+    queryset = Burger.objects.filter(is_active=True)
+
+    @extend_schema(
+        summary="Lista dos Hamburguers",
+    )
+    def get(self, request, *args, **kwargs):
+        """Retorna a lista de pedidos de hamburgers"""
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Cria um Hamburguer",
+    )
+    def post(self, request, *args, **kwargs):
+        """Cria de pedido de um hamburgers"""
+        return super().post(request, *args, **kwargs)
 
 
 class BurgerRUD(RetrieveUpdateDestroyAPIView):
     serializer_class = BurgerSerializer
-    queryset = Burger.objects.all()
+    queryset = Burger.objects.filter(is_active=True)
+
+    @extend_schema(
+        summary="Le um Hamburguers",
+    )
+    def get(self, request, *args, **kwargs):
+        """Le um hamburger pelo id"""
+        return super().get(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Autalização parcial de um Hamburguers",
+    )
+    def patch(self, request, *args, **kwargs):
+        """Atualiza um hamburger pelo id"""
+        return super().patch(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Autalização de um Hamburguers",
+    )
+    def put(self, request, *args, **kwargs):
+        """Atualiza um hamburger pelo id"""
+        return super().put(request, *args, **kwargs)
+
+    @extend_schema(
+        summary="Deleta um Hamburguers",
+    )
+    def delete(self, request, *args, **kwargs):
+        """Deleta um hamburger pelo id"""
+        return super().delete(request, *args, **kwargs)
 
 
 ingredientes_list = IngredientesList.as_view()
